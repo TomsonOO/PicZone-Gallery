4bb019ae132798d2e426fc93e4a6982b42128550
TomsonOO <tomasz.rzepka52gmail.com>
2024-04-11 19:14:19 +0200

    basic frontend structure added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+        <div className="flex flex-col bg-green-100 shadow-lg min-h-screen">
+                <Navbar className=""/>
+                <main className="flex-grow">
+                    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                        </Routes>
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="top-0 w-full bg-green-200  shadow-md z-10">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between h-16">
+                    Navbar
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
