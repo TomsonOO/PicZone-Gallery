ed4d64a40607ae93d9d5db837a4dd88dc9d362c0
TomsonOO <tomasz.rzepka52gmail.com>
2024-04-23 12:09:08 +0200

    created openapi documentation for endpoints

--- backend/src/Controller/ImageController.php
+++ backend/src/Controller/ImageController.php
@@ -2,18 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
 use App\Repository\ImageRepository;
 use Aws\S3\S3Client;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 class ImageController extends AbstractController
 {
 
     #[Route('/images/presigned-url/{objectKey}', name: 'image_presigned_url')]
-    public function getPresignedUrl(string $objectKey): Response
+    #[OA\Get(
+        path: "/images/presigned-url/{objectKey}",
+        operationId: "getPresignedUrl",
+        description: "Generates a presigned URL for directly accessing an image from AWS S3 without needing AWS credentials.",
+        summary: "Get AWS S3 presigned URL",
+        tags: ["Image"],
+        parameters: [
+            new OA\Parameter(
+                name: "objectKey",
+                description: "The S3 object key for the image",
+                in: "path",
+                required: true,
+                schema: new OA\Schema(type: "string")
+            ),
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Presigned URL returned successfully",
+                content: new OA\JsonContent(
+                    type: "string",
+                    example: "https://examplebucket.s3.amazonaws.com/example.jpg?AWSAccessKeyId=..."
+                )
+            ),
+            new OA\Response(
+                response: 500,
+                description: "Error generating presigned URL",
+                content: new OA\JsonContent(
+                    type: "string",
+                    example: "Error generating presigned URL"
+                )
+            ),
+        ]
+    )] public function getPresignedUrl(string $objectKey): Response
     {
         $s3Client = new S3Client([
             'version' => 'latest',
@@ -39,6 +75,24 @@ class ImageController extends AbstractController
 }
 
     #[Route('/api/images', name: 'api_images', methods: ['GET'])]
+    #[OA\Get(
+        path: "/api/images",
+        operationId: "listImages",
+        description: "Retrieves a list of images that are marked to be shown on the homepage.",
+        summary: "List images",
+        tags: ["Image"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "List of images",
+                content: new OA\JsonContent(
+                    type: 'array',
+                    items: new OA\Items(ref: new Model(type: Image::class)),
+
+                ),
+            )
+        ]
+    )]
     public function listImages(ImageRepository $imageRepository): JsonResponse
     {
         $images = $imageRepository->findBy(['showOnHomepage' => true]);
@@ -53,7 +107,6 @@ class ImageController extends AbstractController
         }, $images);
 
         return $this->json($data);
-
     }
 
 
