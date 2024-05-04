a1b10780e8c69ef6d45a3cd2d998cd9b26b5bac1
TomsonOO <tomasz.rzepka52gmail.com>
2024-05-04 15:37:36 +0200

    migration fix

--- backend/migrations/Version20240424165831.php
+++ backend/migrations/Version20240424165831.php
@@ -14,20 +14,18 @@ final class Version20240424165831 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add type column to image table.';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE image (id INT NOT NULL, filename VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, show_on_homepage BOOLEAN NOT NULL, object_key VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN image.created_at IS \'(DC2Type:datetime_immutable)\'');
+        // Add only the new column if it is indeed new and necessary.
+        $this->addSql('ALTER TABLE image ADD type VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP TABLE image');
+        // Provide a way to revert the change.
+        $this->addSql('ALTER TABLE image DROP COLUMN type');
     }
 }
