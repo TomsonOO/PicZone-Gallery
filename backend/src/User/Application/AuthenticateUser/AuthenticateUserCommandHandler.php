2f36f6d121e0eae38456039134c6ec65ed35a421
TomsonOO <tomasz.rzepka52@gmail.com>
2024-10-14 10:54:27 +0200

    refactored UserAuthenticator according to hexagonal architecture structure

---
+++ backend/src/User/Application/AuthenticateUser/AuthenticateUserCommandHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\User\Application\AuthenticateUser;
+
+use App\User\Application\Port\UserRepositoryPort;
+use App\User\Application\Port\JWTTokenManagerPort;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+class AuthenticateUserCommandHandler
+{
+    private UserRepositoryPort $userRepository;
+    private UserPasswordHasherInterface $passwordHasher;
+    private JWTTokenManagerPort $jwtManager;
+
+    public function __construct(UserRepositoryPort $userRepository, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerPort $jwtManager)
+    {
+        $this->userRepository = $userRepository;
+        $this->passwordHasher = $passwordHasher;
+        $this->jwtManager = $jwtManager;
+    }
+
+    public function handle(AuthenticateUserCommand $command): string
+    {
+        $user = $this->userRepository->loadUserByIdentifier($command->getUsername());
+
+        if (!$user) {
+            throw new AuthenticationException('Username not found');
+        }
+
+        if (!$this->passwordHasher->isPasswordValid($user, $command->getPassword())) {
+            throw new AuthenticationException('Incorrect password');
+        }
+
+        return $this->jwtManager->createToken($user);
+    }
+}
---
+++ backend/src/User/Infrastructure/Adapter/JWTTokenManagerAdapter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\User\Infrastructure\Adapter;
+
+use App\User\Application\Port\JWTTokenManagerPort;
+use App\User\Domain\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+
+class JWTTokenManagerAdapter implements JWTTokenManagerPort
+{
+    private JWTTokenManagerInterface $jwtManager;
+
+    public function __construct(JWTTokenManagerInterface $jwtManager)
+    {
+        $this->jwtManager = $jwtManager;
+    }
+
+    public function createToken(User $user): string
+    {
+        return $this->jwtManager->create($user);
+    }
+}
