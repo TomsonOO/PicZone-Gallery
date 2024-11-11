48409bffa72de6200e68c049f99207c67c870032
TomsonOO <tomasz.rzepka52@gmail.com>
2024-11-11 10:25:52 +0100

    added unit test for the user validation

--- backend/src/User/Application/CreateUser/CreateUserCommand.php
+++ backend/src/User/Application/CreateUser/CreateUserCommand.php
@@ -7,36 +7,39 @@ use App\User\Application\Validator\Constraints as AppAssert;
 
 class CreateUserCommand
 {
-    #[Assert\NotBlank(message: "Username is required.")]
+    #[Assert\NotBlank(message: 'Username is required.')]
     #[Assert\Length(
         min: 6,
         max: 20,
-        minMessage: "Username must be at least {{ limit }} characters long.",
-        maxMessage: "Username cannot be longer than {{ limit }} characters."
+        minMessage: 'Username must be at least {{ limit }} characters long.',
+        maxMessage: 'Username cannot be longer than {{ limit }} characters.',
     )]
     #[AppAssert\UniqueUsername]
     private string $username;
 
-    #[Assert\NotBlank(message: "Email is required.")]
-    #[Assert\Email(message: "The email '{{ value }}' is not a valid email.")]
+    #[Assert\NotBlank(message: 'Email is required.')]
+    #[Assert\Email(
+        message: 'The email {{ value }} is not a valid email.',
+        mode: 'html5'
+    )]
     private string $email;
 
-    #[Assert\NotBlank(message: "Password is required.")]
+    #[Assert\NotBlank(message: 'Password is required.')]
     #[Assert\Length(
         min: 8,
-        minMessage: "Password must be at least {{ limit }} characters long."
+        minMessage: 'Password must be at least {{ limit }} characters long.'
     )]
     #[Assert\Regex(
-        pattern: "/[A-Z]/",
-        message: "Password must contain at least one uppercase letter."
+        pattern: '/[A-Z]/',
+        message: 'Password must contain at least one uppercase letter.'
     )]
     #[Assert\Regex(
-        pattern: "/[a-z]/",
-        message: "Password must contain at least one lowercase letter."
+        pattern: '/[a-z]/',
+        message: 'Password must contain at least one lowercase letter.'
     )]
     #[Assert\Regex(
-        pattern: "/[0-9]/",
-        message: "Password must contain at least one number."
+        pattern: '/[0-9]/',
+        message: 'Password must contain at least one number.'
     )]
     private string $password;
 
