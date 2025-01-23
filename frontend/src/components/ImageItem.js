00be4dd1f72ae2df95d6666e3748a77b7e0223af
TomsonOO <tomasz.rzepka52@gmail.com>
2025-01-23 11:21:38 +0100

    feat: implemented adding images to favorites on frontend, modified how liking image is handled on frontend

--- frontend/src/components/ImageItem.js
+++ frontend/src/components/ImageItem.js
@@ -3,9 +3,7 @@ import ImageModal from './modals/ImageModal';
 import FavoriteButton from './buttons/FavoriteButton';
 import LikeButton from './buttons/LikeButton';
 import { toast } from 'react-toastify';
-import likeImage from '../services/likeImage';
-
-import 'react-toastify/dist/ReactToastify.css';
+import { toggleLike, toggleFavorite } from '../services/imageActions';
 
 let toastId = null;
 
@@ -21,17 +19,13 @@ export default function ImageItem({ image }) {
     setUserLiked(image.liked);
   }, [image.liked]);
 
-  async function handleLike() {
+  async function handleToggleLike() {
     const token = localStorage.getItem('token');
     if (!token) {
       if (!toastId) {
-        const isDarkMode = window.matchMedia(
-          '(prefers-color-scheme: dark)'
-        ).matches;
         toastId = toast.error('You must be logged in to like images.', {
           position: 'top-right',
-          autoClose: 5000,
-          className: `toast-notification`,
+          autoClose: 3000,
           onClose: () => {
             toastId = null;
           },
@@ -41,19 +35,38 @@ export default function ImageItem({ image }) {
     }
     setAnimating(true);
     setUserLiked(!userLiked);
-    setLikeCount((prev) => (userLiked ? prev - 1 : prev + 1));
+    setLikeCount(prev => (userLiked ? prev - 1 : prev + 1));
     try {
-      await likeImage(image.id, process.env.REACT_APP_BACKEND_URL);
+      await toggleLike(image.id, process.env.REACT_APP_BACKEND_URL);
     } catch {
       setUserLiked(userLiked);
-      setLikeCount((prev) => (userLiked ? prev + 1 : prev - 1));
+      setLikeCount(prev => (userLiked ? prev + 1 : prev - 1));
     } finally {
       setAnimating(false);
     }
   }
 
-  function handleFavorite() {
+  async function handleToggleFavorite() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      if (!toastId) {
+        toastId = toast.error('You must be logged in to favorite images.', {
+          position: 'top-right',
+          autoClose: 3000,
+          className: 'toast-notification',
+          onClose: () => {
+            toastId = null;
+          },
+        });
+      }
+      return;
+    }
     setIsFavorite(!isFavorite);
+    try {
+      await toggleFavorite(image.id, process.env.REACT_APP_BACKEND_URL);
+    } catch {
+      setIsFavorite(isFavorite);
+    }
   }
 
   function openModal() {
@@ -65,57 +78,57 @@ export default function ImageItem({ image }) {
   }
 
   return (
-    <div className='relative group mb-4'>
-      {image.url ? (
-        <>
-          <div
-            className={`relative overflow-hidden transition-colors duration-300 ${
-              imgLoaded ? '' : 'bg-white dark:bg-gray-800'
-            }`}
-          >
-            <img
-              src={image.url}
-              alt={image.description || 'Image'}
-              loading='lazy'
-              onLoad={() => setImgLoaded(true)}
-              onClick={openModal}
-              className={`w-full h-auto object-cover transition-all duration-300 ${
-                imgLoaded ? 'opacity-100' : 'opacity-0'
-              } hover:scale-105 cursor-pointer`}
-            />
-          </div>
-          <div className='absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-70 transition-opacity duration-300 p-2 flex justify-between'>
-            <p className='text-white text-sm break-words max-w-[70%]'>
-              {image.description}
-            </p>
-            <div className='flex items-center space-x-2 justify-end'>
-              <FavoriteButton
-                isFavorite={isFavorite}
-                onToggleFavorite={handleFavorite}
-              />
-              <LikeButton
-                userLiked={userLiked}
-                likeCount={likeCount}
-                animating={animating}
-                onLike={handleLike}
-              />
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className='animate-pulse bg-gray-300 w-full h-60' />
-      )}
-      <ImageModal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        imageUrl={image.url}
-        userLiked={userLiked}
-        likeCount={likeCount}
-        animating={animating}
-        isFavorite={isFavorite}
-        onLike={handleLike}
-        onFavorite={handleFavorite}
-      />
-    </div>
+      <div className="relative group mb-4">
+        {image.url ? (
+            <>
+              <div
+                  className={`relative overflow-hidden transition-colors duration-300 ${
+                      imgLoaded ? '' : 'bg-white dark:bg-gray-800'
+                  }`}
+              >
+                <img
+                    src={image.url}
+                    alt={image.description || 'Image'}
+                    loading="lazy"
+                    onLoad={() => setImgLoaded(true)}
+                    onClick={openModal}
+                    className={`w-full h-auto object-cover transition-all duration-300 ${
+                        imgLoaded ? 'opacity-100' : 'opacity-0'
+                    } hover:scale-105 cursor-pointer`}
+                />
+              </div>
+              <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-70 transition-opacity duration-300 p-2 flex justify-between">
+                <p className="text-white text-sm break-words max-w-[70%]">
+                  {image.description}
+                </p>
+                <div className="flex items-center space-x-2 justify-end">
+                  <FavoriteButton
+                      isFavorite={isFavorite}
+                      onToggleFavorite={handleToggleFavorite}
+                  />
+                  <LikeButton
+                      userLiked={userLiked}
+                      likeCount={likeCount}
+                      animating={animating}
+                      onLike={handleToggleLike}
+                  />
+                </div>
+              </div>
+            </>
+        ) : (
+            <div className="animate-pulse bg-gray-300 w-full h-60" />
+        )}
+        <ImageModal
+            isOpen={isModalOpen}
+            onClose={closeModal}
+            imageUrl={image.url}
+            userLiked={userLiked}
+            likeCount={likeCount}
+            animating={animating}
+            isFavorite={isFavorite}
+            onLike={handleToggleLike}
+            onFavorite={handleToggleFavorite}
+        />
+      </div>
   );
 }
